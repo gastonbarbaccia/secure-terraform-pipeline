c4436cf9f100c8d0fc3960cec858eb79e57ddf44
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2026-01-17 19:08:29 -0300

    ok

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,24 +25,24 @@ variable "db_password" {
 
 
 variable "ec2_name" {
-  default = "dojo_sonar-13"
+  default = "dojo_sonar-14"
 }
 
 variable "sg_name_ec2" {
-  default = "sg_dojo_sonar_ec2-13"
+  default = "sg_dojo_sonar_ec2-14"
 }
 
 
 variable "sg_name_rds" {
-  default = "sg_dojo_sonar_rds-13"
+  default = "sg_dojo_sonar_rds-14"
 }
 
 variable "secure_subnet_name" {
-  default = "subnet_dojo_sonar-13"
+  default = "subnet_dojo_sonar-14"
 }
 
 variable "db_name" {
-  default = "app-postgres-db-13"
+  default = "app-postgres-db-14"
 }
 
 variable "key_pair_name" {
