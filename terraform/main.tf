fd6cbe4acb8c88d8f6957c66ad22cbc1871e28c9
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2026-01-17 15:19:46 -0300

    ok

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,8 +91,25 @@ resource "aws_instance" "ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
+              set -e
+
               apt update -y
-              apt upgrade -y
+              apt install ca-certificates curl gnupg lsb-release -y
+
+              install -m 0755 -d /etc/apt/keyrings
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+              chmod a+r /etc/apt/keyrings/docker.gpg
+
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+              $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+
+              apt update -y
+              apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+              systemctl enable docker
+              systemctl start docker
+
+              usermod -aG docker ubuntu
               EOF
 }
 
